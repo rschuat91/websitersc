15aa16c165d129772d555369561da9adb0ae6069
rschuat91 <rachael_simba4@hotmail.com>
2022-03-20 16:32:01 +0800

    Create footer.php

---
+++ starter_for_josh_site/footer.php
@@ -0,0 +1,29 @@
+
+
+<footer class="container-fluid text-center">
+   <div class="container">
+     <div class="row">
+<div class="col-md-6">
+   <a class="navbar-brand" href="#"> <span class="josh">JOSH </span> <span class="orange"> WHITKIN </span></a>
+    </div>
+
+    <div class="col-md-6">
+        <p>TERMS AND CONDITIONS</p>
+         </div>
+
+     </div> 
+
+
+  </div> 
+
+  </header>
+  
+       </div>
+
+     </div>
+
+  </footer>
+
+<php wp_footer (); ?>
+</body>
+</html>
