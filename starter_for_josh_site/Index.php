55de3dfba956cf9e8e438a19ab4c66dd065fab30
rschuat91 <rachael_simba4@hotmail.com>
2022-03-20 16:30:10 +0800

    Create Index.php

---
+++ starter_for_josh_site/Index.php
@@ -0,0 +1,109 @@
+
+<php get_header() ;?> <!-- < Tells WordPress to include header. php>  -->
+ <header class="container-fluid bg1">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#"> <span class="josh">JOSH </span> <span class="orange"> WHITKIN </span></a>
+      
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+             <li class="nav-item">
+               <a class="nav-link" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">PROJECTS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PUBLICATION</a>
+            </li> 
+            <li class="nav-item">
+               <a class="nav-link" href="#">CV</a>
+           </li> 
+
+           <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+        </li> 
+           
+
+          </ul>
+        </div> 
+      </nav>
+
+      <div class="container">
+
+         <div class="tag">
+        <h1>GAMES FOR THE FUTURE</h1>
+
+         </div>
+
+<section class= "container-fluid aboutbg text-center">
+
+    <div class="container">
+ <h2> GAMES &amp; LEVEL DESIGNER</h2>
+ <p class="particle"> .... and particle wizard</p>
+ <p class="about-text"> My name is Josh Whitkin. I'm a professional with more than 7 years of industry experience in Games Design,
+     Level Design , Scripting and Visual Effects. What makes me a passionate about games is the combination  of 
+     technology and creativity, and how multiple disciplines come together to create an interactive experience. 
+     Whether I'm desigining for puzzle games, 2D platformers and 3D enviroments.
+       I always tell a story.</p>
+
+
+       <div class="blocker"></div>
+    </div>
+</section>
+
+
+<section class="container-fluid articlesbg">
+  <div class="container">
+   <h3 class="text-center"> LATEST ARTICLES</h3>
+     <div class="row">
+         <div>
+         
+         </div>
+            <div class="col-md-4">
+             <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ portrait.jpg" alt="woman">
+                <h4 class="articletitle"> A PORTRAIT </h4>
+                    <p class="date"> 12 December 2020</p>
+                    <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars 
+                        are starting to explore and intervene in the arts. This object of study brings new challenges 
+                        and opportunities that we want to explore in this session. We would like to gather first of 
+                        all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                 <a class="readmore" href="#" >CONTINUE READING</a>
+            </div>
+
+            <div class="col-md-4">
+               <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg" alt="woman">
+                <h4 class="article title"> A PORTRAIT</h4>
+                <p class="date"> 12 December 2020</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars 
+                    are starting to explore and intervene in the arts. This object of study brings new challenges 
+                    and opportunities that we want to explore in this session. We would like to gather first of 
+                    all simply new kinds of knowledge arising from STS study of the arts.</p>
+                 <a class="readmore" href="#" > CONTINUE READING</a>
+            </div>
+
+
+         <div class="col-md-4">
+        <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg" alt="woman">
+         <h4 class="article title"> A PORTRAIT</h4>
+         <p class="date"> 12 December 2020</p>
+
+         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars 
+            are starting to explore and intervene in the arts. This object of study brings new challenges 
+            and opportunities that we want to explore in this session. We would like to gather first of 
+            all simply new kinds of knowledge arising from STS study of the arts.</p>
+         <a class="readmore" href="#" > CONTINUE READING</a>
+
+
+     </div> 
+-
+</section> 
+
+<php get_footer ();?> <!-- < Tells WordPress to include header. php>  -->
